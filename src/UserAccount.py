1cebf8cf08a0115447749712b1190f8dd9da9775
Nathan Tyler Yu <141592668+nathanyu1@users.noreply.github.com>
2024-03-13 23:20:04 -0400

    Created unfinished userAccount class

---
+++ src/UserAccount.py
@@ -0,0 +1,12 @@
+import json
+
+class UserAccount:
+
+    def __init__(self, userID, userPassword):
+        self.ID = userID
+        self.password = userPassword
+
+    def validateLogin(userID, userPassword):
+        
+
+    
