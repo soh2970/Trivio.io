b805f51a7fc140c3912167082a618dcd48f5f11d
Nathan Tyler Yu <141592668+nathanyu1@users.noreply.github.com>
2024-03-18 17:19:18 -0400

    fixed Boss class with self variables and moved bossHP out of constructor

--- src/Boss.py
+++ src/Boss.py
@@ -3,31 +3,32 @@
 # The boss class
 class Boss:
 
+    bossHp = 100
     # private variables to hold the bosses' hp and the current level
     # constructor to make a new boss with starting hp as 100.
-    def __init__():
-        bossHp = 100
+    def __init__(self):
+        pass
         
     
     # Method to make the boss lose hp.
     # @param level, the boss losses a certain amount of hp depending on the current level
-    def loseBossHP(level):
+    def loseBossHP(self, level):
         if (level == 1):
-            bossHp -= 4
+            self.bossHp -= 4
     
         elif (level == 2):
-            bossHp -= 6
+            self.bossHp -= 6
         
         elif (level == 3):
-            bossHp -= 10
+            self.bossHp -= 10
         
         else:
             raise Exception('This level does not exist.')
     
     # Method to check if the boss of the game has been defeated.
     # @return boolean, true if defeated and false if not.
-    def isBossDefeated():
-        if( bossHp <= 0):
+    def isBossDefeated(self):
+        if ( self.bossHP <= 0):
             return True
         
         else:
