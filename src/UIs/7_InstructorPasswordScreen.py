271831edaaff0d191eea48ea0a5dc13e21d1c196
Michael Kim <gkim297@uwo.ca>
2024-03-24 01:07:36 +0000

    edited

--- src/UIs/7_InstructorPasswordScreen.py
+++ src/UIs/7_InstructorPasswordScreen.py
@@ -4,7 +4,6 @@ Instructor Password Screen
 """
 from screen import ScreenBase
 import pygame
-<<<<<<< HEAD
 import sys
 import os
 
@@ -13,10 +12,8 @@ src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 sys.path.append(src_dir)
 # initializing the constructor 
 pygame.init() 
-=======
 
 class InstructorPassScreen(ScreenBase):
->>>>>>> e098bfe87fe38a37d5ef00d8c3d2ffc00acc5fa8
 
     def __init__(self):
         super().__init__()
