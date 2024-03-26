29a5425841a1b965b9a1d2de9c9a47808ee345b9
Sophia <syuan225@uwo.ca>
2024-03-26 17:05:06 -0400

    sophias options changes

--- src/UIs/13_OptionsScreen.py
+++ src/UIs/13_OptionsScreen.py
@@ -5,6 +5,7 @@ Add a reset button
 
 """
 from screen import ScreenBase
+from AudioManager import AudioManager
 import pygame
 import os
 import sys
@@ -13,30 +14,21 @@ import sys
 src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 sys.path.append(src_dir)
 
+audio_manager = AudioManager()
+
 class OptionsScreen(ScreenBase):
 
     def __init__(self):
         super().__init__()
-        # get the current width and height of the screen
-        self.width = self.screen.get_width()
-        self.height = self.screen.get_height()
 
-        """
-        need to find way to add list or dictionary with the song choices
-        """
-
-        
-
-        #slider values
-        self.slider_pos = self.width/5*3  # Initial slider position
-        self.slider_max = self.width/5*4  # Maximum x-coordinate for the slider
-        self.slider_min = self.width/5*2  # Minimum x-coordinate for the slider
-        self.slider_y = self.height/12*5    # Y-coordinate for the slider
-        self.is_dragging = False  # Track whether the slider is being dragged
-
+        self.vol_slider = slider(self.screen.get_width()/5*2, self.screen.get_width()/5*4, self.screen.get_height()/12*5, self.screen.get_width()/5*3)
 
     def draw(self):
         super().draw()
+
+        # get the current width and height of the screen
+        self.width = self.screen.get_width()
+        self.height = self.screen.get_height()
 
         #text
         self.text1 = self.MEDIUM_FONT.render('Options', True, self.BLACK)
@@ -48,23 +40,24 @@ class OptionsScreen(ScreenBase):
         self.brightness_text = self.BUTTON_FONT.render('DISPLAY', True, self.BLACK)
         self.brightness_textrect = self.brightness_text.get_rect(center = (self.width/5, self.height/12*9))
         
-        #slider
-        
-        # Draw the slider bar
-        pygame.draw.line(self.screen, self.BLACK, (self.slider_min, self.slider_y), (self.slider_max, self.slider_y), 3)
-        # Draw the slider button
-        pygame.draw.circle(self.screen, self.BLACK, (self.slider_pos, self.slider_y), 10)
-        
+        # slider
+        pygame.draw.line(self.screen, self.BLACK, (self.vol_slider.x1, self.vol_slider.height), (self.vol_slider.x2, self.vol_slider.height), 3)
+        # slider button
+        pygame.draw.circle(self.screen, self.BLACK, (self.vol_slider.pos, self.vol_slider.height), 10)
         
         #arrows
+        self.leftArrow = ArrowButton(self.width/5*2, self.height/12*7, 50,50,'left')
+        self.rightArrow = ArrowButton(self.width/5*4, self.height/12*7, 50, 50, 'right')
 
         #buttons
+        
         self.light_button = pygame.draw.rect(self.screen, self.BLACK, (self.width/5*2, self.height/15*10.5, 160,60),1)
         self.light_button_text = self.SMALLER_FONT.render("Light", (self.light_button.centerx, self.light_button.centery), self.BLACK)
         self.light_button_textrect = self.light_button_text.get_rect(center = self.light_button.center)
         self.dark_button = pygame.draw.rect(self.screen, self.DARKGREY, (self.width/5*3, self.height/15*10.5, 160,60))
         self.dark_button_text = self.SMALLER_FONT.render("Dark", (self.dark_button.centerx, self.dark_button.centery), self.WHITE)
         self.dark_button_textrect = self.dark_button_text.get_rect(center = self.dark_button.center)
+        back = self.BUTTON_FONT.render('Back' , True , 0) 
 
         # display text
         self.screen.blit(self.text1, self.textRect1)
@@ -76,13 +69,29 @@ class OptionsScreen(ScreenBase):
         self.screen.blit(self.light_button_text, self.light_button_textrect)
         self.screen.blit(self.dark_button_text, self.dark_button_textrect)
 
+        #display arrows
+       # left_arrow_image_path= ''
+        #left_arrow_image = pygame.image.load(left_arrow_image_path)
+       # left_arrow_image_resized = pygame.transform.scale(boss2_image, (80,80))
+        #right_arrow_image_path = ''
+        #right_arrrow_image = pygame.image.load(boss2_image_path)
+        #right_arrow_image_resized = pygame.transform.scale(boss2_image, (80,80))
 
+
+        self.screen.blit(self.leftArrow.image, self.leftArrow.rect)
+        self.screen.blit(self.rightArrow.image, self.rightArrow.rect)
+        
 
     def handle_events(self):
         # stores the (x,y) coordinates into the variable as a tuple
         self.mouse = pygame.mouse.get_pos() 
 
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        self.vol_slider.slider_events(events)
+        self.leftArrow.arrow_clicks(events)
+        self.rightArrow.arrow_clicks(events)
+
+        for event in events:
             # user quits
             if event.type == pygame.QUIT: 
                 self.running = False
@@ -91,53 +100,74 @@ class OptionsScreen(ScreenBase):
             # user resizing screen
             elif event.type == pygame.VIDEORESIZE:
                 super().resize_screen(event)
-            # user clicking
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if abs(event.pos[0] - self.slider_pos) < 10 and abs(event.pos[1] - self.slider_y) < 10:
-                    self.is_dragging = True
-            elif event.type == pygame.MOUSEBUTTONUP:
-                self.is_dragging = False
-            elif event.type == pygame.MOUSEMOTION and self.is_dragging:
-                self.slider_pos = min(max(event.pos[0], self.slider_min), self.slider_max)
-
-    def adjust_volume(self):
-        volume = (self.slider_pos - self.slider_min) / (self.slider_max - self.slider_min)
-        pygame.mixer.music.set_volume(volume)
+                self.vol_slider.update_slider_pos(self.screen.get_width(),self.screen.get_height())
     
     def run(self):
         super().run()
 
+class slider:
+    def __init__(self, low, high, h, initial_pos):
+        self.x1 = low # left slider x-coord
+        self.x2 = high # right sldier x-coord
+        self.height = h # slider y value
+        self.pos = initial_pos
+        self.colour = (0,0,0)
+        self.is_dragging = False  # Track whether the slider is being dragged
+    
+    def update_slider_pos(self, width, height):
+        relative_slider_pos = (self.pos - self.x1) / (self.x2 - self.x1)
+        self.x2= width/5*4 
+        self.x1 = width/5*2  
+        self.height = height/12*5
+        self.pos = self.x1 + relative_slider_pos * (self.x2 - self.x1)
+
+    def slider_events(self, events):
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if abs(event.pos[0] - self.pos) < 10 and abs(event.pos[1] - self.height) < 10:
+                    self.is_dragging = True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.is_dragging = False
+                self.adjust_volume()
+            elif event.type == pygame.MOUSEMOTION and self.is_dragging:
+                self.pos = min(max(event.pos[0], self.x1), self.x2)
+
+    def adjust_volume(self):
+        volume = (self.pos - self.x1) / (self.x2 - self.x1)
+        audio_manager.set_volume(volume)
+
 class ArrowButton:
-    def __init__(self, colour, x, y, width, height, direction) -> None:
-        self.image = pygame.Surface([width, height])
-        self.image.fill((255,255,255))
+    def __init__(self, x, y, width, height, direction) -> None:
+        self.image = pygame.Surface([width, height],pygame.SRCALPHA)
+        self.colour = (255,255,255)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.rect.width = width
         self.rect.height = height
         self.direction = direction  # 'left' or 'right'
-
+    
+    def draw_arrow(self):
+        points = []
+        if self.direction == 'left':
+            points = [(self.rect.width, 0), (0, self.rect.height//2), (self.rect.width, self.rect.height)]
+        elif self.direction == 'right':
+            points = [(0,0), (self.rect.width, self.rect.height//2), (0, self.rect.height)]
+        
+        self.image.fill((0, 0, 0, 0))
+        pygame.draw.polygon(self.image, self.colour, points)
+    
     def arrow_clicks(self, events):
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.direction=='left' and (self.rect.width-self.rect.x) <= pygame.mouse.get_pos <= self.rect.width and (self.rect.height-self.rect.y) <= pygame.mouse.get_pos() <= self.rect.height:
-                    self.current_song_index = (self.current_song_index - 1) % len(songs)
-                    self.change_song()
-                elif self.direction=='right' and self.rect.width <= pygame.mouse.get_pos() <= (self.rect.width+self.rect.x) and self.rect.height <= pygame.mouse.get_pos() <= (self.rect.height+self.rect.y):
-                    self.current_song_index = (self.current_song_index - 1) % len(songs)
-    
-    def change_song(self):
-        pygame.mixer.music.stop()
-        current_song = songs[self.current_song_index]['path']
-        pygame.mixer.music.load(current_song)
-        pygame.mixer.music.play(-1)
-
-    def draw_current_song(self):
-        song_name = songs[self.current_song_index]['name']
-        font = pygame.font.SysFont(None, 24)
-        text_surface = font.render(song_name, True, (255,255,255))
-        self.screen.blit(text_surface, (100, 50))
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                #for left arrow
+                if self.direction=='left':
+                    if (self.rect.x <= mouse_x <= self.rect.x + self.rect.width and self.rect.y <= mouse_y <= self.rect.y + self.rect.height):
+                        audio_manager.next_song()
+                elif self.direction=='right':
+                    if (self.rect.x <= mouse_x <= self.rect.x + self.rect.width and self.rect.y <= mouse_y <= self.rect.y + self.rect.height):
+                        audio_manager.prev_song()
 
 
 #initialize instance and run
