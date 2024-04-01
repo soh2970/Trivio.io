5c1ff2a238720e6a0b5af2071d2ee1fb32980e59
Sophia <syuan225@uwo.ca>
2024-04-01 12:43:32 -0400

    tutorial screen

--- src/UIs/5_TutorialScreen1.py
+++ src/UIs/5_TutorialScreen1.py
@@ -3,6 +3,7 @@ Tutorial Screen 1
 
 """
 from screen import ScreenBase
+from GameScreenButtons import GameScreenButtons
 import pygame
 import os
 import sys
@@ -25,7 +26,7 @@ class GameTutorialScreen(ScreenBase):
         #images
         self.gamescreen_image_rect = self.gamescreen_imageResized.get_rect(center = (self.width/13*4, self.height/7*3))
         self.screen.blit(self.gamescreen_imageResized, self.gamescreen_image_rect)
-        self.gametext_image_rect = self.gametext_imageResized.get_rect(center = (self.width/5*4, self.height/7*3))
+        self.gametext_image_rect = self.gametext_imageResized.get_rect(center = (self.width/26*21, self.height/7*3))
         self.screen.blit(self.gametext_imageResized, self.gametext_image_rect)
 
         #text heading
@@ -34,7 +35,10 @@ class GameTutorialScreen(ScreenBase):
         self.screen.blit(self.text1, self.textRect1)
         
         #buttons
-        
+        self.next_button = GameScreenButtons(self.width/25*19, self.height/25*16, 140,40, "Next Tutorial", lambda: self.choiceMade(), self.BLUE, self.BLACK)
+        self.next_button.draw(self.screen)
+        self.done_button = GameScreenButtons(self.width/25*19, self.height/25*18, 140,40, "Done Tutorial", lambda: self.choiceMade(), self.GREEN, self.BLACK)
+        self.done_button.draw(self.screen)
 
 
     def handle_events(self):
@@ -53,7 +57,7 @@ class GameTutorialScreen(ScreenBase):
     #load the instructions image
     gametext_image_path = os.path.join(images_dir, 'tutorial_text_1.png')
     gametext_image = pygame.image.load(gametext_image_path)
-    gametext_imageResized = pygame.transform.scale(gametext_image, (250,220))
+    gametext_imageResized = pygame.transform.scale(gametext_image, (318,239))
 
 #initialize instance and run
 if __name__ == '__main__':
