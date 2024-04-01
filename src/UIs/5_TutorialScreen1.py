48168b8e729a30ab6dd3dad42c4da9c73595117d
Sophia <syuan225@uwo.ca>
2024-04-01 04:31:51 -0400

    tut

--- src/UIs/5_TutorialScreen1.py
+++ src/UIs/5_TutorialScreen1.py
@@ -4,14 +4,37 @@ Tutorial Screen 1
 """
 from screen import ScreenBase
 import pygame
+import os
+import sys
+
+images_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'images')
 
 class GameTutorialScreen(ScreenBase):
 
     def __init__(self):
-        super().__init__()
+        super().__init__(self.MIN_WIDTH, self.MIN_HEIGHT)
+
 
     def draw(self):
         super().draw()
+        # get the current width and height of the screen
+        self.width = self.screen.get_width()
+        self.height = self.screen.get_height()
+
+
+        #images
+        self.gamescreen_image_rect = self.gamescreen_imageResized.get_rect(center = (self.width/13*4, self.height/7*3))
+        self.screen.blit(self.gamescreen_imageResized, self.gamescreen_image_rect)
+        self.gametext_image_rect = self.gametext_imageResized.get_rect(center = (self.width/5*4, self.height/7*3))
+        self.screen.blit(self.gametext_imageResized, self.gametext_image_rect)
+
+        #text heading
+        self.text1 = self.SMALLER_FONT.render('How to Play Tutorial', True, self.BLACK)
+        self.textRect1 = self.text1.get_rect(center = (self.width//2, self.height/12))
+        self.screen.blit(self.text1, self.textRect1)
+        
+        #buttons
+
 
     def handle_events(self):
         # call parent class event handling
@@ -19,6 +42,17 @@ class GameTutorialScreen(ScreenBase):
 
     def run(self):
         super().run()
+
+
+    #load the game screen image
+    gamescreen_image_path = os.path.join(images_dir, 'Tutorial_Screen.png')
+    gamescreen_image = pygame.image.load(gamescreen_image_path)
+    gamescreen_imageResized = pygame.transform.scale(gamescreen_image, (563,400))
+
+    #load the instructions image
+    gametext_image_path = os.path.join(images_dir, 'tutorial_text_1.png')
+    gametext_image = pygame.image.load(gametext_image_path)
+    gametext_imageResized = pygame.transform.scale(gametext_image, (250,220))
 
 #initialize instance and run
 if __name__ == '__main__':
